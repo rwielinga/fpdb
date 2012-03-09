d0a2434130e984954af9484c56395db5813391ba
Robert Wielinga <rwielinga@atoi.nl>
2012-03-09 16:09:47 +0100

    Summary files don't break PacificPoker bulk import anymore. Solution similar to FTP, this doesn't bulk import the summary files though.

--- pyfpdb/PacificPokerToFpdb.py
+++ pyfpdb/PacificPokerToFpdb.py
@@ -462,3 +462,7 @@ class PacificPoker(HandHistoryConverter):
 
         log.info("Pacific.getTableTitleRe: returns: '%s'" % (regex))
         return regex
+
+    def readSummaryInfo(self, summaryInfoList):
+        self.status = True
+        return self.status
